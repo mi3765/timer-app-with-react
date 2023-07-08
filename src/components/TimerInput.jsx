58d3561db617fc387c7a60ed0d9432f78e19d63b
mi3765 <chips1225@outlook.jp>
2023-07-08 20:48:06 +0900

    inputに入力された値の取得機能を追加

--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TimerInput.css";
 
-function TimerInput() {
+const TimerInput = () => {
+  const [days, setDays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+
+  const addTime = () => {
+    console.log(days);
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+  };
   return (
     <div>
       <div>
         <section>
           <p>Days </p>
-          <input type="text" placeholder="May 5" />
+          <input
+            type="text"
+            placeholder="May 5"
+            onChange={(e) => setDays(e.target.value)}
+          />
         </section>
         <section>
           <p>Hours</p>
-          <input type="text" placeholder="12" />
+          <input
+            type="text"
+            placeholder="12"
+            onChange={(e) => setHours(e.target.value)}
+          />
         </section>
         <section>
           <p>Minutes</p>
-          <input type="text" placeholder="35" />
+          <input
+            type="text"
+            placeholder="35"
+            onChange={(e) => setMinutes(e.target.value)}
+          />
         </section>
         <section>
           <p>Seconds</p>
-          <input type="text" placeholder="45" />
+          <input
+            type="text"
+            placeholder="45"
+            onChange={(e) => setSeconds(e.target.value)}
+          />
         </section>
+        <button onClick={addTime}>setTime</button>
       </div>
     </div>
   );
-}
+};
 
 export default TimerInput;
