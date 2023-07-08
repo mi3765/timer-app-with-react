6d5faa2318621c3402024b23c8c6cf4936f31a37
mi3765 <chips1225@outlook.jp>
2023-07-08 17:37:27 +0900

    TimerInputコンポーネントを表示

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Timer from "./components/Timer";
+import TimerInput from "./components/TimerInput";
 
 function App() {
   return (
     <div className="App">
+      <TimerInput></TimerInput>
       <Timer></Timer>
     </div>
   );
